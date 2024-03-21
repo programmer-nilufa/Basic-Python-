d7df2a7fbb0e9be134feefd977183ed0947fb679
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-03-22 02:13:58 +0600

    Random_modul

---
+++ Random_modul.py
@@ -0,0 +1,36 @@
+import random
+
+        #randint random
+"""
+a = random.randint(1,6)
+print(a)
+"""
+        #randrange random
+"""
+a = random.randrange(1,7)
+print(a)
+"""
+        #random random
+"""
+a = random.random()
+print(a)
+"""
+        #uniforn random
+"""
+a = random.uniform(1,3)
+print(a)
+"""
+        #choice random
+"""
+num = [1,-5,6,2]
+random.choice(num)
+print(num)
+"""
+
+        #shuffle random
+"""
+num = [1,-5,6,2]
+random.shuffle(num)
+print(num)
+"""
+
